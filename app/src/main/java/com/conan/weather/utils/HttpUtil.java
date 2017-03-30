5b176701b8a1f3b0300c9055af3b6b3b5c056a6d
conan <673665678@qq.com>
2017-03-30 10:44:42 +0800

    增加 网络请求日志打印

--- app/src/main/java/com/conan/weather/utils/HttpUtil.java
+++ app/src/main/java/com/conan/weather/utils/HttpUtil.java
@@ -1,5 +1,11 @@
 package com.conan.weather.utils;
 
+import com.conan.weather.BuildConfig;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
@@ -15,7 +21,17 @@ public class HttpUtil {
 
     private static String MAIN_URL = "http://guolin.tech/";
     public static HttpService http() {
+        OkHttpClient.Builder builder = new OkHttpClient().newBuilder();
+        builder.readTimeout(10, TimeUnit.SECONDS);
+        builder.connectTimeout(9, TimeUnit.SECONDS);
+
+        if (BuildConfig.DEBUG) {
+            HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+            interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+            builder.addInterceptor(interceptor);
+        }
         Retrofit retrofit = new Retrofit.Builder()
+                .client(builder.build())
                 .baseUrl(MAIN_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
